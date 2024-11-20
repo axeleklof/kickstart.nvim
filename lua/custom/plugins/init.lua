85269a18883c6df6ef5feaabbebead1de11667bd
Axel Eklöf <axeleklof2@gmail.com>
2024-11-20 09:57:52 +0100

    using catppuccin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,28 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    opts = {
+      flavour = 'mocha',
+      default_integrations = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        neotree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = '',
+        },
+      },
+    },
+    init = function()
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+}
